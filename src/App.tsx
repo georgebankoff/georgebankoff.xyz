75eeec447e6de9977c5b6e7af5f247eb43e7f7c9
georgebankoff <122503423+georgebankoff@users.noreply.github.com>
2023-02-05 19:06:49 -0500

    tweaked stuff, messing with default code

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,11 @@ function App() {
           <img src="/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React hi</h1>
+      <h1>Website website websitE</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
