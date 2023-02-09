e74d22e958b33150a1efbefaf9eee27fac2440b1
georgebankoff <122503423+georgebankoff@users.noreply.github.com>
2023-02-09 14:21:50 -0500

    this doesn't work. I'm trying to add pages and a navbar. I think it's something to do with the imports, maybe DOM import

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,20 @@
-import React from 'react'
-import { useState } from 'react'
-import './App.css'
+import * as React from "https://esm.sh/react@18.2.0";
+import {
+  Navigate,
+  Route,
+  Routes,
+} from "https://esm.sh/react-router-dom@6.8.1?deps=react@18.2.0";
+import { NavBar } from "./components/NavBar.tsx";
+import { HomePage } from "./pages/HomePage.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App(props) {
   return (
-    <div className="App">
-      {/* <img src="/vite-deno.svg" alt="Vite with Deno" />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src="/react.svg" className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1 className = "header">
-        <span className="clip">
-          weeeeebsite
-        </span>
-      </h1>
-      <div>
-        <img src="/Wanderer.webp" className = "wanderer"/>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+    <div>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
-  )
+  );
 }
-
-export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import * as React from "https://esm.sh/react@18.2.0";
+import { useState } from 'https://esm.sh/react@18.2.0'
+import '../App.css'
+
+export function HomePage() {
+  const [count, setCount] = useState(0)
+
+    return (
+      <div className="App">
+        {/* <img src="/vite-deno.svg" alt="Vite with Deno" />
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src="/react.svg" className="logo react" alt="React logo" />
+          </a>
+        </div> */}
+        <h1 className = "header">
+          <span className="clip">
+            weeeeebsite
+          </span>
+        </h1>
+        <div>
+          <img src="/Wanderer.webp" className = "wanderer"/>
+        </div>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+        {/* <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p> */}
+      </div>
+    )
+}
+
+export default HomePage
