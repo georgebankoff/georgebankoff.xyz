392d79476b61dcddad2227fe44f591dbc68420ad
georgebankoff <gbankoff24@sidwell.edu>
2023-02-09 23:19:51 -0500

    line breaks in poem

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -7,7 +7,7 @@ export function BlogPage() {
       <h1>weblog</h1>
       <p className = 'paragraph'>
         {`
-        Turning and turning in the widening\n
+        Turning and turning in the widening gyre
         The falcon cannot hear the falconer;
         Things fall apart; the centre cannot hold;
         Mere anarchy is loosed upon the world,
