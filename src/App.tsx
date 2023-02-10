e665cfe7518d82cf03b6a0c5dbe09b2ab1ae204b
georgebankoff <gbankoff24@sidwell.edu>
2023-02-09 23:13:12 -0500

    things fall apart

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import { NavBar } from "./components/NavBar";
 import { HomePage } from "./pages/HomePage";
 import './App.css'
+import { BlogPage } from "./pages/Blog.tsx";
 
 
 export default function App(props) {
@@ -15,6 +16,7 @@ export default function App(props) {
       <NavBar />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/blog" element={<BlogPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
