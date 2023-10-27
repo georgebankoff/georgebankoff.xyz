dcfa9fd33c6ffb7940c833f397a4edfdf81de662
georgebankoff <122503423+georgebankoff@users.noreply.github.com>
2023-10-26 20:47:12 -0400

    idk what im doing

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,27 @@ export function HomePage() {
         <p>
           I made this website using React to post photos that I took on a trip to Maine with Babak Tafreshi.
         </p>
-        <div className = "spaced">
-          <img src="/dipper-trail.webp" className='wanderer'></img>
+          <img src="/dipper-trail.webp" className='image'></img>
           <p className="description">
             This is a composite star trail photo of the sky. You can see Ursa Major (the Big Dipper) on the bottom left, and the stars rotating around Polaris in the upper right.
           </p> 
-          <img src="/moon.webp" className='wanderer'></img>
+          <img src="/moon.webp" className='image'></img>
           <p className="description">
             This is a is a picture of the Moon, Jupiter, and Pleiades.
             </p> 
-          <img src="/moon-trail.webp" className='wanderer'></img>
+          <img src="/moon-trail.webp" className='image'></img>
           <p className="description">
               This is a composite star trail photo of the same frame of the previous photo over a couple hours. You can see the Moon rising. I had gone to bed before the Moon had started to rise, so I wasn't able to adjust the exposure to make the Moon clearer.
             </p> 
-          <img src="/long.webp" className='wanderer'></img>
+          <img src="/long.webp" className='image'></img>
           <p className="description">
               This is a 30-minute exposure of the sky, with Polaris just about in the center of the frame.
             </p> 
-          <img src="/eddie-exposed.webp" className='wanderer'></img>
+          <img src="/eddie-exposed.webp" className='image'></img>
           <p className="description">
               This is a light painting of me with an artistic representation of my dog, Eddie, in front of the Milky Way. It is a 30-second exposure.
             </p> 
 
-        </div>
       </div>
     )
 }
