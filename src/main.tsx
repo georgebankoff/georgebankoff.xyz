602b514b31292d71febdb805a91ff24592123f41
George Bankoff <122503423+georgebankoff@users.noreply.github.com>
2025-10-10 12:15:12 -0400

    Upgrade Deno and clean up deployment code
    Here are the key changes we made:
    - Upgrade Deno setup action to v2.x
    - Remove unnecessary deploy includes
    - Simplify deploy.ts file server code
    - Clean up React imports and formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import {
-  BrowserRouter
-} from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import App from "./App";
-import './index.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 // Bind react app to <div id="app" />
 const root = ReactDOM.createRoot(document.getElementById("root"));
