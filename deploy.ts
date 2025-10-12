4a46c460fa64a19e284c8017fb294290a4446da0
George Bankoff <122503423+georgebankoff@users.noreply.github.com>
2025-10-12 16:50:50 -0400

    revert

--- deploy.ts
+++ deploy.ts
@@ -1,12 +1,16 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.177.0/http/file_server.ts
 
+<<<<<<< HEAD
 
 
   ,
 ";
 
 serve(req => {
+=======
+serve((req) => {
+>>>>>>> parent of 303ba8e (Revert "Upgrade Deno and clean up deployment code")
   return serveDir(req, {
     fsRoot: "dist",
   });
